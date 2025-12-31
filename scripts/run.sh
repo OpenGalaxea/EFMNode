c7cc59365b27b4e251f48f0293f7544b23714df1
yue.sun <yue.sun@galaxea.ai>
2025-12-31 14:40:31 +0800

    [bugfix] add LD_LIBRARY_PATH for Tensorrt

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,5 +7,6 @@ export HF_ENDPOINT="https://hf-mirror.com"
 export PYTORCH_JIT_LOG_LEVEL='profiling_graph_executor_impl'
 export HYDRA_FULL_ERROR=1
 export PYTHONPATH="${SCRIPT_DIR}/../:${PYTHONPATH:-}"
+export LD_LIBRARY_PATH=/data/TensorRT-10.13.0.35/lib/:/usr/lib/x86_64-linux-gnu/:$LD_LIBRARY_PATH
 
 python3 "${SCRIPT_DIR}/../run.py"
